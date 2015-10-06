483ba1160d7f26fe2bd212996c9f11a0cab18bab
Nicole <Nicole.Ahima@gmail.com>
2015-10-05 21:12:38 -0500

    String.Split

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
 {
@@ -10,8 +11,26 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            /*User enters equation
+              Program should read equation by reading all characters until a character is an operator
+              save characters before operator in a variable that stores them as intergers
+              save operator in a variable as operator
+              continue reading after operator and save characters in a variable as intergers
+              perform the equation
+              writeline the answer
+              look up: parsing input
+               */
             Console.WriteLine("Enter your equation");
-            string equation = (Console.ReadLine()); 
+            string expressions = (Console.ReadLine());
+
+            string pattern = @"(\d+)\s+([-+*/])\s+(\d+)";
+
+            foreach (var expression in expressions)
+                foreach (Match m in Regex.Matches(expression, pattern))
+                {
+                    int value1 = Int32.Parse(m.Groups[1].Value);
+                    int value2 = Int32.Parse(m.Groups[3].Value);
+                    switch (m.Groups[2].Value)
 
             Console.WriteLine("Write a number for x");
             int x = Convert.ToInt32(Console.ReadLine());
