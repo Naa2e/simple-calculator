46aa57f6e767f5e9c985b9dfad1b1e94793f0fe7
Nicole <Nicole.Ahima@gmail.com>
2015-10-05 20:28:27 -0500

    Commit

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,8 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your equation");
+            string equation = (Console.ReadLine()); 
 
             Console.WriteLine("Write a number for x");
             int x = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +24,7 @@ namespace SimpleCalculator
             Console.WriteLine("2 - subtration ");
             Console.WriteLine("3 - multification");
             Console.WriteLine("4 - division");
+            //Console.WriteLine("5 - percentage");
 
             int z = Convert.ToInt32(Console.ReadLine());
             switch (z)
@@ -38,9 +41,32 @@ namespace SimpleCalculator
                 case 4:
                     Console.WriteLine(x / y);
                     break;
+                /*case 5:
+                    Console.WriteLine(x / y);
+                    break;*/
             }
             Console.ReadKey(true);
         }
+
+        /*Console.WriteLine("Enter your number of friends");
+          int x = Convert.ToInt32(Console.ReadLine());
+          if x = 2 alive
+          if x = 3 alive
+          if x >= 4 dead
+
+            {
+                case 1:
+                   if x = 1 Console.WriteLine(dead);
+                break;
+                case 2:
+                    if x = 2 Console.WriteLine(alive);
+                break;
+                case 3:
+                    if x = 3 Console.WriteLine(alive);
+                break;
+                case 4:
+                    if x = 4 Console.WriteLine(dead);
+                break;*/
     }
-    }
+}
     
